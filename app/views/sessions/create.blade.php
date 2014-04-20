4d74bae42b67369fba9f06edde42c3c76a68c8ce
Andrew Morgan <andrewmile@gmail.com>
2014-04-19 23:34:19 -0400

    clean up password page

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.master')
 
-{{ Form::open(['route' => 'sessions.store']) }}
+@section('content')
+	{{ Form::open(['route' => 'sessions.store']) }}
 
-	<div class="form-group">
-		{{ Form::label('email', 'email') }}
-		{{ Form::text('email', $value = null, array('class' => 'form-control')) }}
-		{{ $errors->first('email') }}
-	</div>
+		<div class="form-group">
+			{{ Form::label('email', 'email') }}
+			{{ Form::text('email', $value = null, array('class' => 'form-control')) }}
+			{{ $errors->first('email') }}
+		</div>
 
-	<div class="form-group">
-		{{ Form::label('password', 'Password') }}
-		{{ Form::password('password', $value = null, array('class' => 'form-control')) }}
-		{{ $errors->first('password') }}
-	</div>
+		<div class="form-group">
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password', array('class' => 'form-control')) }}
+			{{ $errors->first('password') }}
+		</div>
 
-	{{ Form::submit(isset($button_text) ? $button_text : 'login', ['class' => 'btn btn-primary']) }}
+		{{ Form::submit(isset($button_text) ? $button_text : 'login', ['class' => 'btn btn-primary']) }}
 
-{{ Form::close() }}
+	{{ Form::close() }}
+@stop
